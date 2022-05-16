81d6e3a910b14765d57991293355a6a37ec4359b
Farid Hidayat <farid.hidayat@sagara.id>
2022-05-16 16:04:49 +0700

    feat: modalTaskScreen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -13,6 +13,7 @@ import { ColorSchemeName, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
+import ModalTaskScreen from '../screens/ModalTaskScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
@@ -42,6 +43,7 @@ function RootNavigator() {
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
+        <Stack.Screen name="TaskDetail" component={ModalTaskScreen} />
       </Stack.Group>
     </Stack.Navigator>
   );
